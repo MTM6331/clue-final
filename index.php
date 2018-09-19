d7d4fc83b78c61cde726922fd730bac82f7dd5b6
Michael Eisenbraun <eisenbm@algonquincollege.com>
2018-09-18 23:04:53 -0400

    Adds final version of files.

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+    $rooms = [
+      'Study' => [
+        'items' => ['Mrs. White', 'the Rope'
+        ]
+      ],
+      'Hall' => [
+        'items' => ['the Lead Pipe']
+      ], 
+      'Lounge' => [
+        'items' => ['Professor Plum']
+      ],
+      'Library' => [
+        'items' => ['the Candlestick']
+      ],
+      'Dining Room' => [
+        'items' => ['Mrs. Peacock']
+      ],
+      'Billard Room' => [
+        'items' => ['Colonel Mustard', 'the Wrench']
+      ],
+      'Conservatory' => [
+        'items' => []
+      ],      
+      'Ballroom' => [
+        'items' => ['Mr. Green']
+      ],
+      'Kitchen' => [
+        'items' => ['the Knife']
+      ]
+    ];
+
+    if (isset($_GET['room']) && isset($rooms[$_GET['room']])) {
+      $items = $rooms[$_GET['room']]['items'];
+    } 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Clue</title>
+  <link rel="stylesheet" href="clue.css">
+</head>
+<body>
+  
+  <header>
+    <h1>Clue</h1>
+  </header>
+  <div class="items"> 
+      <?php if (isset($items)) : ?>
+        <?php if (count($items)) : ?>
+        <p>You found <?php echo implode(' and ', $items); ?>  
+        <?php else : ?>
+        <p>You found no items.</p>
+        <?php endif; ?>
+      <?php else : ?>
+        <p>Select a room to get a clue.</p>
+      <?php endif; ?>
+  </div>
+  <main class="container">
+    <?php foreach ($rooms as $room => $items) : ?>
+      <div class="room">
+        <a href="?room=<?php echo $room; ?>"><?php echo $room; ?></a>
+      </div>
+    <?php endforeach; ?>
+  </main>
+</body>
+</html>
